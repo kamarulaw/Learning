fbc5d5b067772043aea88c21005d0e6213b7d322
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-04-05 11:11:18 -0400

    Update algiers.pl

--- Perl/algiers.pl
+++ Perl/algiers.pl
@@ -19,18 +19,25 @@ sub math {
     #}
 }
 ######################################################################################################################################
-
 use warnings; 
 use strict; 
 
 sub IndexLevelAverage {
     # Compute the Index-level Average for the five length-ten input arrays
-    my (@arr1, @arr2, @arr3, @arr4, @arr5) = @_; 
-    
+    my ($arr1, $arr2, $arr3, $arr4, $arr5) = @_; 
+    print scalar @$arr1 , "\t"; 
+    print scalar @$arr2 , "\t"; 
+    print scalar @$arr3 , "\t"; 
+    print scalar @$arr4 , "\t"; 
+    print scalar @$arr5 , "\n";
     my @result = ();
     
     for (my $i = 0; $i < 10; $i++) {
-        push(@result, ($arr1[$i] + $arr2[$i] + $arr3[$i] + $arr4[$i] + $arr5[$i]) / 5);
+        my $sum = @$arr1[$i] + @$arr2[$i] + @$arr3[$i] + @$arr4[$i] + @$arr5[$i];
+        
+        my $index_level_average = $sum / 5; 
+        
+        push(@result, $index_level_average);
     }
     print("@result\n");
 }
@@ -50,10 +57,12 @@ foreach my $item (@a1) {
     push(@a5, $item + 4); 
 }
 
-&IndexLevelAverage(@a1, @a2, @a3, @a4, @a5);
+&IndexLevelAverage(\@a1, \@a2, \@a3, \@a4, \@a5)
+
 #my @array_of_arrays = (@a1, @a2, @a3, @a4, @a5);
 #&PrintArray(@array_of_arrays[0]);
 #&PrintArray(@array_of_arrays[1]);
 #&PrintArray(@array_of_arrays[2]);
 #&PrintArray(@array_of_arrays[3]);
 #&PrintArray(@array_of_arrays[4]);
+# HINT: https://www.perlmonks.org/?node_id=862178 (Passint two arrays to a subrroutine)
