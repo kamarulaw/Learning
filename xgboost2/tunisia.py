53524618813463280c11e48ac85f2d94dddc961a
Kamaru-deen Lawal <44913782+kamarulaw@users.noreply.github.com>
2024-04-13 11:27:49 -0400

    Update tunisia.py

--- xgboost2/tunisia.py
+++ xgboost2/tunisia.py
@@ -1,6 +1,11 @@
-#onlinegdb c++ compiler -> python3
+#OnlineGDB [Python]
 
-arr = [1,2,3,4,5,6,7,8,9]
+arr = [0,1,2,3,4,5,6,7,8,9]
 
+result = ""
 for i in range(len(arr)):
-    print(chr(arr[i] + 96))
+    result += str(chr(arr[i] + 96))
+
+f = open("onlinegdb_outputfile.txt", "a")
+f.write(result) 
+f.close()
